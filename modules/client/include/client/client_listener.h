79bd7ab263f1c6fdb297ef65eee55c1ab6f0d8d6
mmathieu <mmathieu@student.ethz.ch>
2024-11-06 21:22:21 +0100

    Client side networking done, not able to test atm though.

---
+++ modules/client/include/client/client_listener.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <functional>
+#include <wx/wx.h>
+#include "sockpp/tcp_socket.h"
+#include "sockpp/tcp_connector.h"
+
+
+class ClientListener : public wxThread {
+
+public:
+    ClientListener(sockpp::tcp_connector* connection);
+    ~ClientListener();
+
+protected:
+    virtual ExitCode Entry();
+
+private:
+    void outputError(std::string title, std::string message);
+
+
+    sockpp::tcp_connector* _connection;
+
+};
---
+++ modules/client/src/client_main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <thread>
+
+#include "client/client_listener.h"
+#include "client/client_network_manager.h"
+
+void set_up_network(std::string host, uint16_t port){
+    ClientNetworkManager network;
+    network.init(host, port);
+}
+
+int main()
+{
+    std::string input, host;
+    uint16_t port;
+    std::cin >> host >> port;
+
+    std::thread thread(set_up_network, host, port);
+    thread.detach();
+
+    input = ".";
+    std::cout << "Initialized!" << std::endl;
+
+    while(input != "exit"){
+        std::cin >> input;
+        //network.sendRequest(input);
+        std::cout << input;
+    }
+    return 0;
+}

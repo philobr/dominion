79e02dae091967f2526a4cf932d61c6c5431cef2
Philipp <pobrist@student.ethz.ch>
2024-11-08 16:18:34 +0100

    Tests for checking max_players

--- unit_tests/server/lobby_lobbymanager.cpp
+++ unit_tests/server/lobby_lobbymanager.cpp
@@ -95,6 +95,8 @@ TEST(ServerLibraryTest, JoinLobby)
     shared::PlayerBase::id_t player_1 = "Max";
     shared::PlayerBase::id_t player_2 = "Peter";
     shared::PlayerBase::id_t player_3 = "Paul";
+    shared::PlayerBase::id_t player_4 = "John";
+    shared::PlayerBase::id_t player_5 = "George";
 
     shared::CreateLobbyRequestMessage request1;
     request1.game_id = "123";
@@ -106,22 +108,41 @@ TEST(ServerLibraryTest, JoinLobby)
     request2.message_id = "789";
     request2.player_id = player_2;
 
-    shared::JoinLobbyRequestMessage request3;
-    request3.game_id = "222";
-    request3.message_id = "101";
-    request3.player_id = player_3;
+    shared::JoinLobbyRequestMessage false_request3;
+    false_request3.game_id = "222";
+    false_request3.message_id = "101";
+    false_request3.player_id = player_3;
+
+    shared::JoinLobbyRequestMessage corrected_request3;
+    corrected_request3.game_id = "123";
+    corrected_request3.message_id = "102";
+    corrected_request3.player_id = player_3;
+
+    shared::JoinLobbyRequestMessage request4;
+    request4.game_id = "123";
+    request4.message_id = "103";
+    request4.player_id = player_4;
+
+    shared::JoinLobbyRequestMessage request5;
+    request5.game_id = "123";
+    request5.message_id = "104";
+    request5.player_id = player_5;
 
     auto games = lobby_manager.get_games();
 
     lobby_manager.create_lobby(request1);
 
-    // All expected function calls of send_message (checked bottom-up)
+    // All expected function calls of send_message
     {
         InSequence s;
         EXPECT_CALL(*message_interface, send_message(Truly(is_join_lobby_broadcast_message), player_1)).Times(1);
         EXPECT_CALL(*message_interface, send_message(Truly(is_success_message), player_2)).Times(1);
         EXPECT_CALL(*message_interface, send_message(Truly(is_failure_message), player_2)).Times(1);
         EXPECT_CALL(*message_interface, send_message(Truly(is_failure_message), player_3)).Times(1);
+        // 1x ResultResponseMessage + 2x JoinLobbyBroadcastMessage 
+        // + 1x ResultResponseMessage + 3x JoinLobbyBroadcastMessage
+        EXPECT_CALL(*message_interface, send_message(_, _)).Times(7); 
+        EXPECT_CALL(*message_interface, send_message(Truly(is_failure_message), player_5)).Times(1);
     }
     lobby_manager.join_lobby(request2);
     ASSERT_EQ(games->at("123").get_full_game_state().get_players().size(), 2)
@@ -135,13 +156,24 @@ TEST(ServerLibraryTest, JoinLobby)
             << "There should still be two players in the lobby";
 
     // Player 3 should not be able to join the lobby with id 123
-    lobby_manager.join_lobby(request3);
+    lobby_manager.join_lobby(false_request3);
     ASSERT_EQ(games->at("123").get_full_game_state().get_players().size(), 2)
             << "There should still be two players in the lobby";
+
+    // Player 3 should be able to join the lobby with id 123
+    lobby_manager.join_lobby(corrected_request3);
+
+    // Player 4 should be able to join the lobby
+    lobby_manager.join_lobby(request4);
+
+    // Player 5 should not be able to join because the lobby is full
+    lobby_manager.join_lobby(request5);
+    ASSERT_EQ(games->at("123").get_full_game_state().get_players().size(), 4)
+            << "There should still be four players in the lobby";
     delete message_interface;
 }
 
 TEST(ServerLibraryTest, StartGame)
 {
-    // TODO implement
+    
 }

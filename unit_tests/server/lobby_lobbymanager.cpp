6af0e604bc382ae46e3cc94b725333b4b47157ce
Philipp <pobrist@student.ethz.ch>
2024-11-08 15:23:06 +0100

    first message tests for CreateLobby

--- unit_tests/server/lobby_lobbymanager.cpp
+++ unit_tests/server/lobby_lobbymanager.cpp
@@ -3,14 +3,29 @@
 #include <shared/game_state.h>
 #include <shared/message_types.h>
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include <typeinfo>
+
+using ::testing::_;
+using ::testing::Truly;
 
 TEST(ServerLibraryTest, CreateLobby)
 {
-    server::MessageInterface message_interface;
-    server::LobbyManager lobby_manager(message_interface);
+    auto is_create_lobby_response_message = [](shared::ServerToClientMessage *message) {
+        shared::CreateLobbyResponseMessage *create_lobby_response_msg = dynamic_cast<shared::CreateLobbyResponseMessage *>(message);
+        return create_lobby_response_msg != nullptr;
+    };
+
+    auto is_failure_message = [](shared::ServerToClientMessage *message) {
+        shared::ResultResponseMessage *result_msg = dynamic_cast<shared::ResultResponseMessage *>(message);
+        return result_msg && !result_msg->success;
+    };
+
+    server::MockMessageInterface *message_interface = new server::MockMessageInterface();
+    server::LobbyManager lobby_manager(*message_interface);
     shared::PlayerBase::id_t player_1 = "Max";
     shared::PlayerBase::id_t player_2 = "Peter";
-
+    
     shared::CreateLobbyRequestMessage request1;
     request1.game_id = "123";
     request1.message_id = "456";
@@ -21,16 +36,21 @@ TEST(ServerLibraryTest, CreateLobby)
     request2.message_id = "789";
     request2.player_id = player_2;
 
+    // All expected function calls of send_message
+    EXPECT_CALL(*message_interface, send_message(Truly(is_failure_message), _)).Times(1); // Error for second time creating lobby
+    EXPECT_CALL(*message_interface, send_message(Truly(is_create_lobby_response_message), _)).Times(1); // Success for first time creating lobby
+
     auto games = lobby_manager.get_games();
     ASSERT_EQ(games->empty(), true) << "LobbyManager should be empty at the beginning";
-
+    
     lobby_manager.create_lobby(request1);
+    
     ASSERT_EQ(games->size(), 1) << "LobbyManager should contain one lobby after creating one";
     ASSERT_EQ(games->find("123") != games->end(), true) << "Lobby with id 123 should exist";
     ASSERT_EQ(games->at("123").get_game_master(), player_1) << "Game master should be player_1";
     ASSERT_EQ(games->at("123").get_full_game_state().get_players().size(), 1)
             << "There should be one player in the lobby";
-
+    
     // No new lobby should be created, because game with id 123 already exists
     lobby_manager.create_lobby(request2);
     ASSERT_EQ(games->size(), 1);
@@ -39,12 +59,14 @@ TEST(ServerLibraryTest, CreateLobby)
     // Check if the game_master didn't change
     ASSERT_EQ(games->at("123").get_game_master(), player_1);
     ASSERT_EQ(games->at("123").get_full_game_state().get_players().size(), 1);
+    
+    delete message_interface;
 }
 
 TEST(ServerLibraryTest, JoinLobby)
 {
-    server::MessageInterface message_interface;
-    server::LobbyManager lobby_manager(message_interface);
+    server::MockMessageInterface *message_interface = new server::MockMessageInterface();
+    server::LobbyManager lobby_manager(*message_interface);
     shared::PlayerBase::id_t player_1 = "Max";
     shared::PlayerBase::id_t player_2 = "Peter";
     shared::PlayerBase::id_t player_3 = "Paul";
@@ -90,6 +112,7 @@ TEST(ServerLibraryTest, JoinLobby)
     lobby_manager.join_lobby(request3);
     ASSERT_EQ(games->at("123").get_full_game_state().get_players().size(), 2)
             << "There should still be two players in the lobby";
+    delete message_interface;
 }
 
 TEST(ServerLibraryTest, StartGame)

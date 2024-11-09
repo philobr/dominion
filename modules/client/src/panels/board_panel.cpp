30776adb30712816e73cfda69bffb073f81aa00e
Caspar Schucan <caspar.schucan@gmail.com>
2024-11-09 12:58:18 +0100

    draw piles on board

--- modules/client/src/panels/board_panel.cpp
+++ modules/client/src/panels/board_panel.cpp
@@ -1,12 +1,20 @@
 #include "board_panel.h"
 #include <wx/wx.h>
+#include "pile_panel.h"
 
 namespace client
 {
 
     BoardPanel::BoardPanel(wxWindow *parent, wxSize size) : wxPanel(parent, wxID_ANY, wxDefaultPosition, size)
     {
-        this->SetBackgroundColour(wxColour(0, 255, 0));
+      wxGridSizer* sizer = new wxGridSizer(3, 10, 0, 0);
+
+      PilePanel* EstatePanel = new PilePanel(this, shared::Pile("Estate", 10));
+      PilePanel* DuchyPanel = new PilePanel(this, shared::Pile("Duchy", 10));
+
+      sizer->Add(EstatePanel, 0, wxALIGN_CENTER_HORIZONTAL);
+      sizer->Add(DuchyPanel, 0, wxALIGN_CENTER_HORIZONTAL);
+      this->SetSizer(sizer);
     }
 
 
---
+++ modules/client/src/panels/pile_panel.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "../uiElements/image_panel.h"
+
+#include <shared/game_state.h>
+
+#include <wx/wx.h>
+
+
+namespace client {
+  class PilePanel : public wxPanel {
+    public:
+      PilePanel(wxWindow* parent, shared::Pile pile);
+    
+  };
+} // namespace client

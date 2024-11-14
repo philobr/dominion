d8e83770cc87ac343d9abd7c36dad406de2e55b9
amazzetta <amazzetta@student.ethz.ch>
2024-11-14 15:57:43 +0100

    added player_base tests, some small refactors

--- modules/shared/src/game/player_base.cpp
+++ modules/shared/src/game/player_base.cpp
@@ -3,13 +3,13 @@
 namespace shared
 {
     PlayerBase::PlayerBase(id_t player_id) :
-        player_id(player_id), victory_points(0), available_actions(1), available_buys(1), available_treasure(0),
+        player_id(player_id), victory_points(0), actions(1), buys(1), treasure(0),
         draw_pile_size(0)
     {}
 
     PlayerBase::PlayerBase(const PlayerBase &other) :
-        player_id(other.player_id), victory_points(other.victory_points), available_actions(other.available_actions),
-        available_buys(other.available_buys), available_treasure(other.available_treasure),
+        player_id(other.player_id), victory_points(other.victory_points), actions(other.actions),
+        buys(other.buys), treasure(other.treasure),
         current_card(other.current_card), discard_pile(other.discard_pile), draw_pile_size(other.draw_pile_size)
     {}
 
@@ -18,9 +18,9 @@ namespace shared
         if ( this != &other ) {
             player_id = other.player_id;
             victory_points = other.victory_points;
-            available_actions = other.available_actions;
-            available_buys = other.available_buys;
-            available_treasure = other.available_treasure;
+            actions = other.actions;
+            buys = other.buys;
+            treasure = other.treasure;
             current_card = other.current_card;
             discard_pile = other.discard_pile;
             draw_pile_size = other.draw_pile_size;
@@ -30,8 +30,8 @@ namespace shared
 
     PlayerBase::PlayerBase(PlayerBase &&other) noexcept :
         player_id(std::move(other.player_id)), victory_points(other.victory_points),
-        available_actions(other.available_actions), available_buys(other.available_buys),
-        available_treasure(other.available_treasure), current_card(std::move(other.current_card)),
+        actions(other.actions), buys(other.buys),
+        treasure(other.treasure), current_card(std::move(other.current_card)),
         discard_pile(std::move(other.discard_pile)), draw_pile_size(other.draw_pile_size)
     {}
 
@@ -39,9 +39,9 @@ namespace shared
     {
         if ( this != &other ) {
             victory_points = other.victory_points;
-            available_actions = other.available_actions;
-            available_buys = other.available_buys;
-            available_treasure = other.available_treasure;
+            actions = other.actions;
+            buys = other.buys;
+            treasure = other.treasure;
             current_card = std::move(other.current_card);
             discard_pile = std::move(other.discard_pile);
             draw_pile_size = other.draw_pile_size;
@@ -51,26 +51,42 @@ namespace shared
 
     void PlayerBase::decActions()
     {
-        if ( available_actions == 0 ) {
+        if ( actions == 0 ) {
             return;
         }
-        available_actions--;
+
+        actions--;
     }
 
     void PlayerBase::decBuys()
     {
-        if ( available_buys == 0 ) {
+        if ( buys == 0 ) {
             return;
         }
-        available_buys--;
+
+        buys--;
     }
 
     void PlayerBase::decTreasure(const unsigned int dec_amount)
     {
-        if ( available_treasure < dec_amount ) {
+        if ( treasure < dec_amount ) {
             return;
         }
-        available_treasure -= dec_amount;
+
+        treasure -= dec_amount;
+    }
+
+    bool PlayerBase::operator==(const PlayerBase &other) const
+    {
+        // this is so cursed, sry
+        return (player_id == other.player_id) && (victory_points == other.victory_points) &&
+                (actions == other.actions) && (buys == other.buys) &&
+                (treasure == other.treasure) && (current_card == other.current_card) &&
+                ((discard_pile.first == other.discard_pile.first) &&
+                 (discard_pile.second == other.discard_pile.second)) &&
+                (draw_pile_size == other.draw_pile_size) &&
+                ((played_cards.size() == other.played_cards.size()) &&
+                 (std::equal(played_cards.begin(), played_cards.end(), other.played_cards.begin())));
     }
 
 } // namespace shared

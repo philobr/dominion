2d5b553d48af4d8ebe00622daa463775c8322023
amazzetta <amazzetta@student.ethz.ch>
2024-11-14 23:44:03 +0100

    created a test_helper file

---
+++ modules/shared/include/shared/utils/test_helpers.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <vector>
+
+#include <shared/game/cards/card_base.h>
+
+std::vector<shared::CardBase::id_t> get_valid_kingdom_cards();
---
+++ modules/shared/src/utils/test_helpers.cpp
@@ -0,0 +1,7 @@
+#include <shared/utils/test_helpers.h>
+
+std::vector<shared::CardBase::id_t> get_valid_kingdom_cards()
+{
+    return {"Village", "Smithy", "Market", "Festival",    "Laboratory",
+            "Moat",    "Chapel", "Feast",  "Moneylender", "Remodel"};
+}

f92d4eacee26db48cda4c942efc5a0767de68e45
amazzetta <amazzetta@student.ethz.ch>
2024-11-14 23:45:41 +0100

    fixed errors from refactor

--- modules/shared/include/shared/game/game_state/reduced_game_state.h
+++ modules/shared/include/shared/game/game_state/reduced_game_state.h
@@ -10,16 +10,15 @@ namespace shared
     {
     public:
         // Constructor to use on the server side
-        ReducedGameState(Board board, ReducedPlayer::ptr_t reduced_player,
+        ReducedGameState(Board::ptr_t board, ReducedPlayer::ptr_t reduced_player,
                          std::vector<ReducedEnemy::ptr_t> &&reduced_enemies, // rvalue ref
                          const PlayerBase::id_t &active_player) :
-            board(std::move(board)),
-            reduced_player(std::move(reduced_player)), reduced_enemies(std::move(reduced_enemies)),
-            active_player(active_player)
+            board(std::move(board)), reduced_player(std::move(reduced_player)),
+            reduced_enemies(std::move(reduced_enemies)), active_player(active_player)
         {}
 
 
-        Board board;
+        Board::ptr_t board;
         const ReducedPlayer::ptr_t reduced_player;
         const std::vector<ReducedEnemy::ptr_t> reduced_enemies;
         PlayerBase::id_t active_player;

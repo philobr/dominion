d325e1dfc7c1d20afcd3d10a99d1fd49c0ff87aa
amazzetta <amazzetta@student.ethz.ch>
2024-11-15 14:19:34 +0100

    added a new test helper

--- modules/shared/include/shared/utils/test_helpers.h
+++ modules/shared/include/shared/utils/test_helpers.h
@@ -3,5 +3,23 @@
 #include <vector>
 
 #include <shared/game/cards/card_base.h>
+#include <shared/game/game_state/board_base.h>
 
+/**
+ * @brief Returns 10 kingdom cards:
+ *
+ * "Village", "Smithy", "Market", "Festival",       "Laboratory",
+ *
+ * "Moat",    "Chapel", "Feast",  "Moneylender",    "Remodel"
+ *
+ * @return std::vector<shared::CardBase::id_t>
+ */
 std::vector<shared::CardBase::id_t> get_valid_kingdom_cards();
+
+/**
+ * @brief Set the count of min(n, pile_container.size()) piles to empty.
+ *
+ * @param pile_container
+ * @param n
+ */
+void set_n_piles_to_empty(shared::Board::pile_container_t &pile_container, size_t n);
--- modules/shared/src/utils/test_helpers.cpp
+++ modules/shared/src/utils/test_helpers.cpp
@@ -5,3 +5,14 @@ std::vector<shared::CardBase::id_t> get_valid_kingdom_cards()
     return {"Village", "Smithy", "Market", "Festival",    "Laboratory",
             "Moat",    "Chapel", "Feast",  "Moneylender", "Remodel"};
 }
+
+void set_n_piles_to_empty(shared::Board::pile_container_t &pile_container, size_t n)
+{
+    size_t i = 0;
+    for ( auto &pile : pile_container ) {
+        if ( i >= n )
+            break;
+        pile.count = 0;
+        ++i;
+    }
+}

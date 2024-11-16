87a8bd0840fecdd2f53617a8b72aa935d95dce2b
darkfireZZ <64635413+darkfireZZ@users.noreply.github.com>
2024-11-16 10:00:08 +0100

    Make clang-tidy happy

--- modules/server/src/basic_network.cpp
+++ modules/server/src/basic_network.cpp
@@ -60,7 +60,7 @@ namespace server
 
     BasicNetwork *BasicNetwork::getInstance()
     {
-        if ( !_instance ) {
+        if ( _instance == nullptr ) {
             _instance = new BasicNetwork();
         }
         return _instance;

24ef90f53ed00381780014ab6ac9c4083ff864b0
darkfireZZ <64635413+darkfireZZ@users.noreply.github.com>
2024-11-18 17:46:53 +0100

    Format code

--- modules/shared/src/action_order.cpp
+++ modules/shared/src/action_order.cpp
@@ -11,22 +11,19 @@ namespace shared
 
     bool ActionOrder::operator!=(const ActionOrder &other) const { return !ActionOrder::operator==(other); }
 
-    bool ActionPhaseOrder::operator==(const ActionPhaseOrder &other) const
-    {
-        return true;
-    }
+    bool ActionPhaseOrder::operator==(const ActionPhaseOrder &other) const { return true; }
 
-    bool ActionPhaseOrder::operator!=(const ActionPhaseOrder &other) const { return !ActionPhaseOrder::operator==(other); }
+    bool ActionPhaseOrder::operator!=(const ActionPhaseOrder &other) const
+    {
+        return !ActionPhaseOrder::operator==(other);
+    }
 
     bool ActionPhaseOrder::equals(const ActionOrder &other) const
     {
         return *this == dynamic_cast<const ActionPhaseOrder &>(other);
     }
 
-    bool BuyPhaseOrder::operator==(const BuyPhaseOrder &other) const
-    {
-        return true;
-    }
+    bool BuyPhaseOrder::operator==(const BuyPhaseOrder &other) const { return true; }
 
     bool BuyPhaseOrder::operator!=(const BuyPhaseOrder &other) const { return !BuyPhaseOrder::operator==(other); }
 
@@ -35,12 +32,12 @@ namespace shared
         return *this == dynamic_cast<const BuyPhaseOrder &>(other);
     }
 
-    bool ChooseNCardsFromHandOrder::operator==(const ChooseNCardsFromHandOrder &other) const
-    {
-        return n == other.n;
-    }
+    bool ChooseNCardsFromHandOrder::operator==(const ChooseNCardsFromHandOrder &other) const { return n == other.n; }
 
-    bool ChooseNCardsFromHandOrder::operator!=(const ChooseNCardsFromHandOrder &other) const { return !ChooseNCardsFromHandOrder::operator==(other); }
+    bool ChooseNCardsFromHandOrder::operator!=(const ChooseNCardsFromHandOrder &other) const
+    {
+        return !ChooseNCardsFromHandOrder::operator==(other);
+    }
 
     bool ChooseNCardsFromHandOrder::equals(const ActionOrder &other) const
     {

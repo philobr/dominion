9d407c2ec46ccc459e66d7adfc8e9487bb273a93
amazzetta <amazzetta@student.ethz.ch>
2024-11-20 13:31:42 +0100

    finished logger cleanup, now to formatting:)

--- modules/shared/include/shared/utils/logger.h
+++ modules/shared/include/shared/utils/logger.h
@@ -55,15 +55,11 @@ namespace shared
     namespace log_helpers
     {
         std::string toString(LogLevel level);
-
-        /**
-         * @brief Adds color codes to the level string, if log_to_file is true.
-         *
-         * @param level
-         * @param log_to_file
-         * @return std::string
-         */
         std::string formatLevel(LogLevel level, bool toggle_colors);
+
+        std::string formatLogLevel(LogLevel level, bool toggle_colors);
+        std::string formatFileLine(const char *file, int line);
+        std::string formatTimestamp();
 
         /**
          * @brief Strips the filepath to be rooted in our root directory
@@ -133,20 +129,10 @@ namespace shared
     // ================================
     Logger::LogStream::LogStream(Logger &logger, LogLevel level, const char *file, int line) : logger_(logger)
     {
-        auto now = std::chrono::system_clock::now();
-        auto time = std::chrono::system_clock::to_time_t(now);
-        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
-
-        ss_ << "[" << std::put_time(std::localtime(&time), "%Y-%m-%d %H:%M:%S") << "." << std::setfill('0')
-            << std::setw(3) << ms.count() << "] " << std::setfill(' ');
-
-        const size_t level_width = (logger.log_to_file_ ? 5 : 16);
-        ss_ << "[" << std::left << std::setw(level_width) << log_helpers::formatLevel(level, logger.log_to_file_)
-            << "] ";
-
-        ss_ << "[" << log_helpers::stripFilePath(file) << ":" << line << "] - ";
+        ss_ << log_helpers::formatTimestamp() << " ";
+        ss_ << log_helpers::formatLogLevel(level, logger.log_to_file_) << " ";
+        ss_ << log_helpers::formatFileLine(file, line) << " - ";
     }
-
 
     // ================================
     // IMPLEMENTATION Logger
@@ -169,7 +155,9 @@ namespace shared
     Logger::~Logger()
     {
         if ( log_file_.is_open() ) {
-            LOG(INFO) << "END LOG" << std::endl;
+            if ( log_to_file_ ) {
+                log_file_ << "[INFO] - END LOG" << std::endl;
+            }
             log_file_.close();
         }
     }
@@ -182,7 +170,9 @@ namespace shared
                 instance_ = std::unique_ptr<Logger>(new Logger());
                 LOG(LogLevel::INFO) << "Logging to std::cerr.";
             } else {
-                const std::string full_path = std::string(PROJECT_ROOT) + std::string("/build/logs/") + file_path;
+                const std::string default_directory = std::string(PROJECT_ROOT) + "/" + std::string("build/logs");
+                const std::string full_path = default_directory + "/" + file_path;
+
                 instance_ = std::unique_ptr<Logger>(new Logger(full_path));
                 std::cout << "Logging to file: build/logs/" + file_path << std::endl;
             }
@@ -260,6 +250,33 @@ namespace shared
                 return file_str.substr(pos + marker.length());
             }
             return file_str; // return full path if marker was not found
+        }
+
+        std::string formatTimestamp()
+        {
+            auto now = std::chrono::system_clock::now();
+            auto time = std::chrono::system_clock::to_time_t(now);
+            auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
+
+            std::ostringstream ss;
+            ss << "[" << std::put_time(std::localtime(&time), "%Y-%m-%d %H:%M:%S") << "." << std::setfill('0')
+               << std::setw(3) << ms.count() << "]";
+            return ss.str();
+        }
+
+        std::string formatLogLevel(LogLevel level, bool log_to_file)
+        {
+            const size_t level_width = (log_to_file ? 5 : 16);
+            std::ostringstream ss;
+            ss << "[" << std::left << std::setw(level_width) << formatLevel(level, log_to_file) << "]";
+            return ss.str();
+        }
+
+        std::string formatFileLine(const char *file, int line)
+        {
+            std::ostringstream ss;
+            ss << "[" << stripFilePath(file) << ":" << line << "]";
+            return ss.str();
         }
     } // namespace log_helpers
 } // namespace shared

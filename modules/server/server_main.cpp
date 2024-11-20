238d30774ab4462e5d9f03d6577ce66945e01af2
amazzetta <amazzetta@student.ethz.ch>
2024-11-20 13:03:53 +0100

    just to test stuff out, will delete later

--- modules/server/server_main.cpp
+++ modules/server/server_main.cpp
@@ -2,10 +2,18 @@
 
 #include "server/network/server_network_manager.h"
 
+#include <shared/utils/test.h>
 int main()
 {
-    // create a message interface, which creates a server network manager, which listens endlessly for connections
-    server::ServerNetworkManager server;
+    // create a message interface, which creates a server network manager, which listens endlessly for
+    // connections server::ServerNetworkManager server;
+    shared::Logger::initialize("test.log");
+    LOG(INFO) << "info?";
+    LOG(WARN) << "test?";
+    LOG(ERROR) << "error?";
+    LOG(DEBUG) << "debug?";
+
+    test("is it working if only defined in main, but called in another file?");
 
     return 0;
 }
---
+++ modules/shared/include/shared/utils/test.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <shared/utils/logger.h>
+
+void test(std::string msg) { LOG(INFO) << msg; }

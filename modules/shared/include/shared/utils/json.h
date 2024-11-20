3a0f3c699523658bc4363b714aebe50719c16c5e
darkfireZZ <64635413+darkfireZZ@users.noreply.github.com>
2024-11-20 12:34:42 +0100

    Improve JSON macros

--- modules/shared/include/shared/utils/json.h
+++ modules/shared/include/shared/utils/json.h
@@ -64,40 +64,40 @@
 
 
 #define ADD_STRING_MEMBER(var, key)                                                                                    \
-    Value key##_value;                                                                                                 \
+    rapidjson::Value key##_value;                                                                                                 \
     key##_value.SetString(var, doc.GetAllocator());                                                                    \
     doc.AddMember(#key, key##_value, doc.GetAllocator());
 
 #define ADD_UINT_MEMBER(var, key)                                                                                      \
-    Value key##_value;                                                                                                 \
+    rapidjson::Value key##_value;                                                                                                 \
     key##_value.SetUint(var);                                                                                          \
     doc.AddMember(#key, key##_value, doc.GetAllocator());
 
 #define ADD_OPTIONAL_STRING_MEMBER(var, key)                                                                           \
     if ( var ) {                                                                                                       \
-        Value key##_value;                                                                                             \
+        rapidjson::Value key##_value;                                                                                             \
         key##_value.SetString(var.value().c_str(), doc.GetAllocator());                                                \
         doc.AddMember(#key, key##_value, doc.GetAllocator());                                                          \
     }
 
 #define ADD_BOOL_MEMBER(var, key)                                                                                      \
-    Value key##_value;                                                                                                 \
+    rapidjson::Value key##_value;                                                                                                 \
     key##_value.SetBool(var);                                                                                          \
     doc.AddMember(#key, key##_value, doc.GetAllocator());
 
 #define ADD_ARRAY_OF_STRINGS_MEMBER(var, key)                                                                          \
-    Value key##_array(kArrayType);                                                                                     \
+    rapidjson::Value key##_array(kArrayType);                                                                                     \
     for ( const auto &item : var ) {                                                                                   \
-        Value item_value;                                                                                              \
+        rapidjson::Value item_value;                                                                                              \
         item_value.SetString(item.c_str(), doc.GetAllocator());                                                        \
         key##_array.PushBack(item_value, doc.GetAllocator());                                                          \
     }                                                                                                                  \
     doc.AddMember(#key, key##_array, doc.GetAllocator());
 
 #define ADD_ARRAY_OF_UINTS_MEMBER(var, key)                                                                            \
-    Value key##_array(kArrayType);                                                                                     \
+    rapidjson::Value key##_array(kArrayType);                                                                                     \
     for ( const auto &item : var ) {                                                                                   \
-        Value item_value;                                                                                              \
+        rapidjson::Value item_value;                                                                                              \
         item_value.SetUint(item);                                                                                      \
         key##_array.PushBack(item_value, doc.GetAllocator());                                                          \
     }                                                                                                                  \

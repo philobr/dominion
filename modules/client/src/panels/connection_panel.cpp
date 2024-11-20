d26cab789dd94729f7e355ab5d5082f8c7131754
Caspar Schucan <caspar.schucan@gmail.com>
2024-11-20 11:08:57 +0100

    satisfy linter

--- modules/client/src/panels/connection_panel.cpp
+++ modules/client/src/panels/connection_panel.cpp
@@ -60,10 +60,12 @@ namespace client
         VerticalLayout->Add(this->_gameName, 0, wxALIGN_CENTER | wxTOP | wxLEFT | wxRIGHT, 10);
 
         wxButton *createLobbyButton = new wxButton(this, wxID_ANY, "create Lobby", wxDefaultPosition, wxSize(100, 40));
-        createLobbyButton->Bind(wxEVT_BUTTON, [](const wxCommandEvent &event) { GameController::connectToServer(); });
+        createLobbyButton->Bind(wxEVT_BUTTON,
+                                [](const wxCommandEvent & /*event*/) { GameController::connectToServer(); });
         VerticalLayout->Add(createLobbyButton, 0, wxALIGN_RIGHT | wxALL, 10);
         wxButton *joinLobbyButton = new wxButton(this, wxID_ANY, "join Lobby", wxDefaultPosition, wxSize(100, 40));
-        joinLobbyButton->Bind(wxEVT_BUTTON, [](const wxCommandEvent &event) { GameController::connectToServer(); });
+        joinLobbyButton->Bind(wxEVT_BUTTON,
+                              [](const wxCommandEvent & /*event*/) { GameController::connectToServer(); });
         VerticalLayout->Add(joinLobbyButton, 0, wxALIGN_RIGHT | wxALL, 10);
 
         this->SetSizerAndFit(VerticalLayout);

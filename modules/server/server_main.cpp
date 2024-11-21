daa5819fd03ddc2555cd9ab5a380adf3ec0fb4d5
amazzetta <amazzetta@student.ethz.ch>
2024-11-21 18:57:06 +0100

    testing

--- modules/server/server_main.cpp
+++ modules/server/server_main.cpp
@@ -1,10 +1,18 @@
 #include <string>
 
 #include "server/network/server_network_manager.h"
+
+#include <server/game/game_state/game_interface.h>
+#include <shared/utils/test_helpers.h>
 int main()
 {
     // create a message interface, which creates a server network manager, which listens endlessly for connections
-    server::ServerNetworkManager server;
+
+    const std::vector<std::string> player_ids = {"one", "two"};
+    const auto kcards = get_valid_kingdom_cards();
+    auto interface = server::GameInterface::make("test", kcards, player_ids);
+
+    //  server::ServerNetworkManager server;
 
     return 0;
 }

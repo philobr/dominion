708da221848500ca2dc7112550eb87c9997250ad
Philipp <pobrist@student.ethz.ch>
2024-11-21 17:56:19 +0100

    Tests for JoinLobby

--- unit_tests/server/lobby_lobbymanager.cpp
+++ unit_tests/server/lobby_lobbymanager.cpp
@@ -32,10 +32,22 @@ MATCHER(IsCreateLobbyResponseMessage, "Checks if the message is CreateLobbyRespo
     return create_lobby_response_msg != nullptr;
 }
 
+MATCHER(IsSuccessMessage, "Checks if the message is a success message")
+{
+    auto result_msg = dynamic_cast<shared::ResultResponseMessage *>(arg.get());
+    return (result_msg != nullptr) && result_msg->success;
+}
+
 MATCHER(IsFailureMessage, "Checks if the message is a failure message")
 {
-    const shared::ResultResponseMessage *result_msg = dynamic_cast<shared::ResultResponseMessage *>(arg.get());
+    auto result_msg = dynamic_cast<shared::ResultResponseMessage *>(arg.get());
     return (result_msg != nullptr) && !result_msg->success;
+}
+
+MATCHER(IsJoinLobbyBroadcastMessage, "Checks if the message is JoinLobbyBroadcastMessage")
+{
+    auto join_lobby_broadcast_msg = dynamic_cast<shared::JoinLobbyBroadcastMessage *>(arg.get());
+    return join_lobby_broadcast_msg != nullptr;
 }
 
 TEST(ServerLibraryTest, CreateLobby)
@@ -80,27 +92,8 @@ TEST(ServerLibraryTest, CreateLobby)
 }
 
 // TODO: Implement tests for the following methods
-/*TEST(ServerLibraryTest, JoinLobby)
+TEST(ServerLibraryTest, JoinLobby)
 {
-    auto is_success_message = [](shared::ServerToClientMessage *message)
-    {
-        const shared::ResultResponseMessage *result_msg = dynamic_cast<shared::ResultResponseMessage *>(message);
-        return (result_msg != nullptr) && result_msg->success;
-    };
-
-    auto is_failure_message = [](shared::ServerToClientMessage *message)
-    {
-        const shared::ResultResponseMessage *result_msg = dynamic_cast<shared::ResultResponseMessage *>(message);
-        return (result_msg != nullptr) && !result_msg->success;
-    };
-
-    auto is_join_lobby_broadcast_message = [](shared::ServerToClientMessage *message)
-    {
-        const shared::JoinLobbyBroadcastMessage *join_lobby_broadcast_msg =
-                dynamic_cast<shared::JoinLobbyBroadcastMessage *>(message);
-        return join_lobby_broadcast_msg != nullptr;
-    };
-
     std::shared_ptr<MockMessageInterface> message_interface = std::make_shared<MockMessageInterface>();
     server::LobbyManager lobby_manager(message_interface);
     shared::PlayerBase::id_t player_1 = "Max";
@@ -109,57 +102,58 @@ TEST(ServerLibraryTest, CreateLobby)
     shared::PlayerBase::id_t player_4 = "John";
     shared::PlayerBase::id_t player_5 = "George";
 
-    shared::CreateLobbyRequestMessage request1("123", "456", player_1);
-    shared::JoinLobbyRequestMessage request2("123", "789", player_2);
-    shared::JoinLobbyRequestMessage false_request3("222", "101", player_3);
-    shared::JoinLobbyRequestMessage corrected_request3("123", "102", player_3);
-    shared::JoinLobbyRequestMessage request4("123", "103", player_4);
-    shared::JoinLobbyRequestMessage request5("123", "104", player_5);
+    auto request1 = std::make_unique<shared::CreateLobbyRequestMessage>("123", uuid_generator::generate_uuid_v4(), player_1);
+    auto request2 = std::make_unique<shared::JoinLobbyRequestMessage>("123", uuid_generator::generate_uuid_v4(), player_2);
+    auto request2_again = std::make_unique<shared::JoinLobbyRequestMessage>("123", uuid_generator::generate_uuid_v4(), player_2);
+    auto false_request3 = std::make_unique<shared::JoinLobbyRequestMessage>("222", uuid_generator::generate_uuid_v4(), player_3);
+    auto corrected_request3 = std::make_unique<shared::JoinLobbyRequestMessage>("123", uuid_generator::generate_uuid_v4(), player_3);
+    auto request4 = std::make_unique<shared::JoinLobbyRequestMessage>("123", uuid_generator::generate_uuid_v4(), player_4);
+    auto request5 = std::make_unique<shared::JoinLobbyRequestMessage>("123", uuid_generator::generate_uuid_v4(), player_5);
 
     const auto *games = lobby_manager.get_games();
 
-    lobby_manager.create_lobby(request1);
+    lobby_manager.create_lobby(std::move(request1));
 
     // All expected function calls of send_message
     {
         InSequence s;
         // request 2
-        EXPECT_CALL(*message_interface, send_message(Truly(is_join_lobby_broadcast_message), player_1)).Times(1);
-        EXPECT_CALL(*message_interface, send_message(Truly(is_success_message), player_2)).Times(1);
+        EXPECT_CALL(*message_interface, send_message(IsJoinLobbyBroadcastMessage(), player_1)).Times(1);
+        EXPECT_CALL(*message_interface, send_message(IsSuccessMessage(), player_2)).Times(1);
         // request 2 again
-        EXPECT_CALL(*message_interface, send_message(Truly(is_failure_message), player_2)).Times(1);
+        EXPECT_CALL(*message_interface, send_message(IsFailureMessage(), player_2)).Times(1);
         // false_request 3
-        EXPECT_CALL(*message_interface, send_message(Truly(is_failure_message), player_3)).Times(1);
+        EXPECT_CALL(*message_interface, send_message(IsFailureMessage(), player_3)).Times(1);
         // corrected_request 3 (1x ResultResponseMessage + 2x JoinLobbyBroadcastMessage)
         // + request 4 (1x ResultResponseMessage + 3x JoinLobbyBroadcastMessage)
         EXPECT_CALL(*message_interface, send_message(_, _)).Times(7);
         // request 5
-        EXPECT_CALL(*message_interface, send_message(Truly(is_failure_message), player_5)).Times(1);
+        EXPECT_CALL(*message_interface, send_message(IsFailureMessage(), player_5)).Times(1);
     }
-    lobby_manager.join_lobby(request2);
+    lobby_manager.join_lobby(std::move(request2));
     ASSERT_EQ(games->at("123")->get_players().size(), 2) << "There should be two players in the lobby";
     ASSERT_EQ(games->at("123")->get_players().at(1), player_2) << "Player 2 should be in the lobby";
 
     // Player 2 should not be added again
-    lobby_manager.join_lobby(request2);
+    lobby_manager.join_lobby(std::move(request2_again));
     ASSERT_EQ(games->at("123")->get_players().size(), 2) << "There should still be two players in the lobby";
 
     // Player 3 should not be able to join the lobby with id 123
-    lobby_manager.join_lobby(false_request3);
+    lobby_manager.join_lobby(std::move(false_request3));
     ASSERT_EQ(games->at("123")->get_players().size(), 2) << "There should still be two players in the lobby";
 
     // Player 3 should be able to join the lobby with id 123
-    lobby_manager.join_lobby(corrected_request3);
+    lobby_manager.join_lobby(std::move(corrected_request3));
 
     // Player 4 should be able to join the lobby
-    lobby_manager.join_lobby(request4);
+    lobby_manager.join_lobby(std::move(request4));
 
     // Player 5 should not be able to join because the lobby is full
-    lobby_manager.join_lobby(request5);
+    lobby_manager.join_lobby(std::move(request5));
     ASSERT_EQ(games->at("123")->get_players().size(), 4) << "There should still be four players in the lobby";
 }
 
-TEST(ServerLibraryTest, StartGame)
+/*TEST(ServerLibraryTest, StartGame)
 {
     auto is_failure_message = [](shared::ServerToClientMessage *message)
     {

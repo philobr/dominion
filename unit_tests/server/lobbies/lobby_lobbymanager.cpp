5bea4309b25861798ce6b41184808ae4ff19b8bc
Philipp <pobrist@student.ethz.ch>
2024-11-22 15:23:10 +0100

    let the tests in the same file but make templates for new tests

--- unit_tests/server/lobbies/lobby_lobbymanager.cpp
+++ unit_tests/server/lobbies/lobby_lobbymanager.cpp
@@ -1,55 +1,10 @@
 #include <server/lobbies/lobby.h>
 #include <server/lobbies/lobby_manager.h>
-#include <server/network/message_interface.h>
+#include "mock_templates.h"
 #include <shared/message_types.h>
 #include <shared/utils/test_helpers.h>
 #include <shared/utils/uuid_generator.h>
 #include <typeinfo>
-
-#include <gmock/gmock.h>
-#include "gtest/gtest.h"
-
-using ::testing::_;
-using ::testing::InSequence;
-using ::testing::Truly;
-
-/**
- * @brief Mock implementation of the MessageInterface
- *
- * @details Used for testing the game logic without actually sending messages to the client
- */
-class MockMessageInterface : public server::MessageInterface
-{
-public:
-    // Mock the send_message method, assuming it takes these parameters
-    MOCK_METHOD(void, send_message,
-                (std::unique_ptr<shared::ServerToClientMessage> message, const shared::PlayerBase::id_t &player_id),
-                (override));
-};
-
-MATCHER(IsCreateLobbyResponseMessage, "Checks if the message is CreateLobbyResponseMessage")
-{
-    auto create_lobby_response_msg = dynamic_cast<shared::CreateLobbyResponseMessage *>(arg.get());
-    return create_lobby_response_msg != nullptr;
-}
-
-MATCHER(IsSuccessMessage, "Checks if the message is a success message")
-{
-    auto result_msg = dynamic_cast<shared::ResultResponseMessage *>(arg.get());
-    return (result_msg != nullptr) && result_msg->success;
-}
-
-MATCHER(IsFailureMessage, "Checks if the message is a failure message")
-{
-    auto result_msg = dynamic_cast<shared::ResultResponseMessage *>(arg.get());
-    return (result_msg != nullptr) && !result_msg->success;
-}
-
-MATCHER(IsJoinLobbyBroadcastMessage, "Checks if the message is JoinLobbyBroadcastMessage")
-{
-    auto join_lobby_broadcast_msg = dynamic_cast<shared::JoinLobbyBroadcastMessage *>(arg.get());
-    return join_lobby_broadcast_msg != nullptr;
-}
 
 TEST(ServerLibraryTest, CreateLobby)
 {

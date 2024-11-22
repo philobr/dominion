7b1f3b24c40995b96caf981a1d1c6e3aa3917a96
darkfireZZ <64635413+darkfireZZ@users.noreply.github.com>
2024-11-22 17:43:13 +0100

    Implement argument parser for server

---
+++ modules/server/include/server/args.h
@@ -0,0 +1,15 @@
+
+#include <shared/utils/logger.h>
+
+namespace server {
+    class ServerArgs {
+    public:
+        ServerArgs(int argc, char *argv[]);
+        ~ServerArgs() = default;
+        std::string getLogFile();
+        LogLevel getLogLevel();
+    private:
+        std::string _logFile;
+        LogLevel _logLevel;
+    };
+}

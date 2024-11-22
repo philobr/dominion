a13929c14d9ee2cbaa7d1e29369560b182e490b7
amazzetta <amazzetta@student.ethz.ch>
2024-11-22 10:33:43 +0100

    initialising logger in server_main

--- modules/server/server_main.cpp
+++ modules/server/server_main.cpp
@@ -4,13 +4,18 @@
 
 #include <server/game/game_state/game_interface.h>
 #include <shared/utils/test_helpers.h>
+
 int main()
 {
+    // initialise the logger to log to build/logs/
+    shared::Logger::initialize("server.log");
+
+    std::vector<std::string> player_ids = {"player1", "player2", "player3"};
+    auto valid_cards = get_valid_kingdom_cards();
+    auto interface = server::GameInterface::make("game_id", valid_cards, player_ids);
+
     // create a message interface, which creates a server network manager, which listens endlessly for connections
-
-    auto behaviour_registry = server::BehaviourRegistry();
-
-    //  server::ServerNetworkManager server;
+    // server::ServerNetworkManager server;
 
     return 0;
 }

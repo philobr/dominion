f33091a6c0fc66d435793cf0c3e926af9338f3e9
darkfireZZ <64635413+darkfireZZ@users.noreply.github.com>
2024-11-22 17:43:41 +0100

    Use argument parser in server

--- modules/server/server_main.cpp
+++ modules/server/server_main.cpp
@@ -1,8 +1,14 @@
 #include <string>
 
+#include "server/args.h"
 #include "server/network/server_network_manager.h"
-int main()
+
+int main(int argc, char* argv[])
 {
+    server::ServerArgs args(argc, argv);
+
+    shared::Logger::initialize(args.getLogFile());
+
     // create a message interface, which creates a server network manager, which listens endlessly for connections
     server::ServerNetworkManager server;
 

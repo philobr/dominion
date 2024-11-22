b99db9e2829e206df1b23cf2ef2681a3f08ea3ac
Philipp <pobrist@student.ethz.ch>
2024-11-22 17:56:50 +0100

    more logging and consistency with player_id and requestor_id

--- modules/server/src/lobby_manager.cpp
+++ modules/server/src/lobby_manager.cpp
@@ -6,7 +6,8 @@ bool server::LobbyManager::lobby_exists(std::string lobby_id) { return games.fin
 void server::LobbyManager::create_lobby(std::unique_ptr<shared::CreateLobbyRequestMessage> request)
 {
     std::string lobby_id = request->game_id;
-    shared::PlayerBase::id_t game_master_id = request->player_id;
+    Player::id_t game_master_id = request->player_id;
+    LOG(INFO) << "LobbyManager::create_lobby called with Lobby ID: " << lobby_id << " and Player ID: " << game_master_id;
     // Lobby already exists
     if ( (games.size() > 0) && lobby_exists(lobby_id) ) {
         LOG(DEBUG) << "Tried creating lobby that already exists. Game ID: " << lobby_id << " , Player ID: " << game_master_id;
@@ -32,7 +33,8 @@ void server::LobbyManager::create_lobby(std::unique_ptr<shared::CreateLobbyReque
 void server::LobbyManager::join_lobby(std::unique_ptr<shared::JoinLobbyRequestMessage> request)
 {
     std::string lobby_id = request->game_id;
-    shared::PlayerBase::id_t player_id = request->player_id;
+    Player::id_t player_id = request->player_id;
+    LOG(INFO) << "LobbyManager::join_lobby called with Lobby ID: " << lobby_id << " and Player ID: " << player_id;
     // Lobby does not exist
     if ( games.find(lobby_id) == games.end() ) {
         LOG(DEBUG) << "Tried joining lobby that does not exist. Game ID: " << lobby_id << " , Player ID: " << player_id;
@@ -49,13 +51,14 @@ void server::LobbyManager::join_lobby(std::unique_ptr<shared::JoinLobbyRequestMe
 void server::LobbyManager::start_game(std::unique_ptr<shared::StartGameRequestMessage> request)
 {
     std::string lobby_id = request->game_id;
-    shared::PlayerBase::id_t requestor_id = request->player_id;
+    Player::id_t player_id = request->player_id;
+    LOG(INFO) << "LobbyManager::start_game called with Lobby ID: " << lobby_id << " and Player ID: " << player_id;
     // Lobby does not exist
     if ( games.find(lobby_id) == games.end() ) {
-        LOG(DEBUG) << "Tried starting game in lobby that does not exist. Game ID: " << lobby_id << " , Player ID: " << requestor_id;
+        LOG(DEBUG) << "Tried starting game in lobby that does not exist. Game ID: " << lobby_id << " , Player ID: " << player_id;
         shared::ResultResponseMessage failure_message =
                 shared::ResultResponseMessage(lobby_id, false, request->message_id, "Lobby does not exist");
-        message_interface->send_message(std::make_unique<shared::ResultResponseMessage>(failure_message), requestor_id);
+        message_interface->send_message(std::make_unique<shared::ResultResponseMessage>(failure_message), player_id);
         return;
     }
 
@@ -64,9 +67,9 @@ void server::LobbyManager::start_game(std::unique_ptr<shared::StartGameRequestMe
 
 void server::LobbyManager::receive_action(std::unique_ptr<shared::ActionDecisionMessage> msg)
 {
-    LOG(INFO) << "Received action from player " << msg->player_id << " in game " << msg->game_id;
+    LOG(INFO) << "LobbyManager::receive_action called with Lobby ID: " << msg->game_id << " and Player ID: " << msg->player_id;
     std::string lobby_id = msg->game_id;
-    shared::PlayerBase::id_t player_id = msg->player_id;
+    Player::id_t player_id = msg->player_id;
     // Lobby does not exist
     if ( !lobby_exists(lobby_id) ) {
         LOG(DEBUG) << "Tried receiving action in lobby that does not exist. Game ID: " << lobby_id << " , Player ID: " << player_id;

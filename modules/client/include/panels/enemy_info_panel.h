7586c7b2f5fbc7fb0846abb2eaace26b197ebc92
Philipp <pobrist@student.ethz.ch>
2024-11-27 16:01:21 +0100

    implement drawEnemies

--- modules/client/include/panels/enemy_info_panel.h
+++ modules/client/include/panels/enemy_info_panel.h
@@ -16,7 +16,7 @@ namespace client
     public:
         EnemyInfoPanel(wxWindow *parent, wxSize size);
 
-        void drawEnemies(const std::vector<std::unique_ptr<reduced::Enemy>> & /*Enemies*/){};
+        void drawEnemies(const std::vector<std::unique_ptr<reduced::Enemy>> &Enemies);
 
     private:
     };
--- modules/client/src/panels/enemy_info_panel.cpp
+++ modules/client/src/panels/enemy_info_panel.cpp
@@ -1,5 +1,6 @@
-
 #include <panels/enemy_info_panel.h>
+#include <shared/utils/logger.h>
+#include <uiElements/enemy_panel.h>
 #include <wx/wx.h>
 
 namespace client
@@ -7,8 +8,26 @@ namespace client
 
     EnemyInfoPanel::EnemyInfoPanel(wxWindow *parent, wxSize size) : wxPanel(parent, wxID_ANY, wxDefaultPosition, size)
     {
-        this->SetBackgroundColour(wxColour(255, 0, 0));
+        // just for temporary testing
+        std::vector<std::unique_ptr<reduced::Enemy>> enemies;
+        for ( int i = 0; i < 3; i++ ) {
+            auto enemy = reduced::Enemy::make(shared::PlayerBase("enemy" + std::to_string(i)), 5);
+            enemies.push_back(std::move(enemy));
+        }
+        drawEnemies(enemies);
     }
 
+    void EnemyInfoPanel::drawEnemies(const std::vector<std::unique_ptr<reduced::Enemy>> &Enemies)
+    {
+        this->DestroyChildren();
 
+        auto *sizer = new wxGridSizer(Enemies.size(), 1, 10, 10);
+
+        for ( const auto &Enemy : Enemies ) {
+            EnemyPanel *enemy_panel = new EnemyPanel(this, *Enemy);
+            sizer->Add(enemy_panel, 0, wxALIGN_CENTER_HORIZONTAL);
+        }
+
+        this->SetSizer(sizer);
+    }
 } // namespace client

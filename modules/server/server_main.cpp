9bffd2d6953ca744c07ab391008bd334e73bb096
amazzetta <amazzetta@student.ethz.ch>
2024-11-28 11:38:29 +0100

    temporarily disabling port passing

--- modules/server/server_main.cpp
+++ modules/server/server_main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
     while ( true ) {
         try {
             server::ServerNetworkManager server;
-            server.run(args.getPort());
+            // server.run();
         } catch ( const std::exception &e ) {
             LOG(ERROR) << "Unhandled exception: " << e.what();
             LOG(DEBUG) << "Restarting server...";

a437f8ec3f4f55b9eb02239ab89894cf35890356
amazzetta <amazzetta@student.ethz.ch>
2024-12-03 11:08:54 +0100

    new choice order

--- modules/shared/include/shared/action_order.h
+++ modules/shared/include/shared/action_order.h
@@ -3,6 +3,8 @@
 
 #include <memory>
 #include <rapidjson/document.h>
+#include <shared/game/cards/card_base.h>
+#include <shared/game/game_state/player_base.h>
 
 namespace shared
 {
@@ -101,6 +103,74 @@ namespace shared
         EndTurnOrder() = default;
         bool operator==(const EndTurnOrder &other) const;
         bool operator!=(const EndTurnOrder &other) const;
+
+    protected:
+        bool equals(const ActionOrder &other) const override;
+    };
+
+    class GainFromBoard : public ActionOrder
+    {
+    public:
+        GainFromBoard(unsigned int max_cost, shared::CardType allowed_type) :
+            max_cost(max_cost), allowed_type(allowed_type)
+        {}
+        bool operator==(const GainFromBoard &other) const;
+        bool operator!=(const GainFromBoard &other) const;
+
+        unsigned int max_cost;
+        shared::CardType allowed_type;
+
+    protected:
+        bool equals(const ActionOrder &other) const override;
+    };
+
+    class ChooseFrom : public ActionOrder
+    {
+    public:
+        enum AllowedChoice
+        {
+            PLAY,
+            TRASH,
+            DISCARD,
+            DRAW_PILE
+        };
+
+        ChooseFrom(unsigned int max_cards, AllowedChoice allowed_choices) :
+            max_cards(max_cards), allowed_choices(allowed_choices)
+        {}
+        bool operator==(const ChooseFrom &other) const;
+        bool operator!=(const ChooseFrom &other) const;
+
+        AllowedChoice allowed_choices;
+        unsigned int max_cards;
+
+    protected:
+        bool equals(const ActionOrder &other) const override;
+    };
+
+    class ChooseFromStaged : public ChooseFrom
+    {
+    public:
+        ChooseFromStaged(std::vector<shared::CardBase::id_t> cards, AllowedChoice choices) :
+            cards(cards), ChooseFrom(cards.size(), choices)
+        {}
+
+        bool operator==(const ChooseFromStaged &other) const;
+        bool operator!=(const ChooseFromStaged &other) const;
+
+        std::vector<shared::CardBase::id_t> cards;
+
+    protected:
+        bool equals(const ActionOrder &other) const override;
+    };
+
+    class ChooseFromHand : public ChooseFrom
+    {
+    public:
+        ChooseFromHand(unsigned int max_cards, AllowedChoice choices) : ChooseFrom(max_cards, choices) {}
+
+        bool operator==(const ChooseFromHand &other) const;
+        bool operator!=(const ChooseFromHand &other) const;
 
     protected:
         bool equals(const ActionOrder &other) const override;

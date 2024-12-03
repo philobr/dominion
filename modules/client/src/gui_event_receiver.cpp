cd0f666a932707643f90230ff6447d1e888c1083
darkfireZZ <64635413+darkfireZZ@users.noreply.github.com>
2024-12-03 12:44:10 +0100

    Fully implement GuiEventReceiver

--- modules/client/src/gui_event_receiver.cpp
+++ modules/client/src/gui_event_receiver.cpp
@@ -11,10 +11,39 @@ namespace client
 
     Gui &GuiEventReceiver::getGui() { return *_gui; }
 
-    void GuiEventReceiver::onControllerEvent(wxThreadEvent & /*event*/)
+    void GuiEventReceiver::onControllerEvent(wxThreadEvent &event)
     {
-        LOG(DEBUG) << "GUI received event from GameController";
-        LOG(WARN) << "OnControllerEvent handler not implemented";
-        // TODO: Implement
+        auto &controllerEvent = static_cast<client::ControllerEvent &>(event);
+
+        switch ( controllerEvent.getType() ) {
+            case ControllerEventType::SHOW_ERROR:
+                {
+                    ShowErrorEventData data = controllerEvent.getData<ShowErrorEventData>();
+                    _gui->showError(std::move(data.title), std::move(data.message));
+                    break;
+                }
+            case ControllerEventType::SHOW_LOBBY_SCREEN:
+                {
+                    ShowLobbyScreenEventData data = controllerEvent.getData<ShowLobbyScreenEventData>();
+                    _gui->showLobbyScreen(std::move(data.players), data.is_game_master);
+                    break;
+                }
+            case ControllerEventType::SHOW_GAME_SCREEN:
+                {
+                    _gui->showGameScreen();
+                    break;
+                }
+            case ControllerEventType::SHOW_VICTORY_SCREEN:
+                {
+                    _gui->showVictoryScreen();
+                    break;
+                }
+            default:
+                {
+                    LOG(ERROR) << "Unknown controller event type: " << static_cast<int>(controllerEvent.getType());
+                    throw exception::UnreachableCode("Unknown controller event type");
+                    break;
+                }
+        }
     }
 } // namespace client

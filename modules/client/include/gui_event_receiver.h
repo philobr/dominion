e8940d4ff3cc72edeb42548738d03f94047d7490
darkfireZZ <64635413+darkfireZZ@users.noreply.github.com>
2024-12-03 10:00:23 +0100

    Add documentation

--- modules/client/include/gui_event_receiver.h
+++ modules/client/include/gui_event_receiver.h
@@ -7,6 +7,11 @@
 
 namespace client
 {
+    /**
+     * @brief The single point of entry for all events that are triggered by the controller
+     *
+     * All interaction with the GUI should be done through this class.
+     */
     class GuiEventReceiver : public wxEvtHandler
     {
     public:
@@ -19,6 +24,11 @@ namespace client
          */
         Gui& getGui();
 
+        /**
+         * @brief This is called by wxWidgets when a controller event is triggered
+         *
+         * @param event The event that was triggered by the controller
+         */
         void onControllerEvent(wxThreadEvent &event);
     private:
         std::unique_ptr<Gui> _gui;

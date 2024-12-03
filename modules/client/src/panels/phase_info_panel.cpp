c0d16023ddadea4b13ab81e602ad369569b33bc7
Philipp <pobrist@student.ethz.ch>
2024-12-03 17:35:16 +0100

    make it look good

--- modules/client/src/panels/phase_info_panel.cpp
+++ modules/client/src/panels/phase_info_panel.cpp
@@ -27,11 +27,11 @@ namespace client
         // Create a grid sizer for the panel
         wxGridSizer* sizer = new wxGridSizer(1, 3, 0, 10);     
 
-
         // Add player info to the sizer
         auto* infoPanel = drawPlayerInfo(game_state.reduced_player);
 
         // Add played cards to the sizer
+        // TODO: switch back to the real played cards
         auto* playedPanel = drawPlayedPanelTest();
         //auto* playedPanel = drawPlayedPanel(game_state.reduced_player->getPlayedCards());
 
@@ -39,9 +39,9 @@ namespace client
         auto* buttonsPanel = drawButtonPanel();
 
         // Add the panels to the sizer
-        sizer->Add(infoPanel, wxSizerFlags().Align(wxALIGN_CENTER_HORIZONTAL).Border(wxALL, 5));
-        sizer->Add(playedPanel, wxSizerFlags().Align(wxALIGN_CENTER_HORIZONTAL).Border(wxALL, 5));
-        sizer->Add(buttonsPanel, wxSizerFlags().Align(wxALIGN_CENTER_HORIZONTAL).Border(wxALL, 5));
+        sizer->Add(infoPanel, wxSizerFlags().Align(wxALIGN_LEFT | wxALIGN_CENTER_VERTICAL).Border(wxALL, 5));
+        sizer->Add(playedPanel, wxSizerFlags().Align(wxALIGN_CENTER).Border(wxALL, 5));
+        sizer->Add(buttonsPanel, wxSizerFlags().Align(wxALIGN_RIGHT | wxALIGN_CENTER_VERTICAL).Border(wxALL, 5));
 
         // Set minimum width for the info bar
         SetMinSize(wxSize(150, -1));  // 150 pixels wide, height automatic
@@ -53,47 +53,40 @@ namespace client
     TextPanel* PhaseInfoPanel::drawPlayerInfo(const std::unique_ptr<reduced::Player>& player)
     {
         wxString info = wxString::Format(
-            "%s\nGold: %d\nActions: %d\nBuys: %d",
+            "%s\n\nGold: %d\n\nActions: %d\n\nBuys: %d",
             player->getId(),
             player->getTreasure(),
             player->getActions(),
             player->getBuys()
         );
 
-        return new TextPanel(this, wxID_ANY, info);
+        return new TextPanel(this, wxID_ANY, info, TextFormat::BOLD);
     }
 
     wxPanel* PhaseInfoPanel::drawPlayedPanel(const std::vector<shared::CardBase::id_t> cards)
     {
         // Get the hand cards
         size_t cards_size = cards.size();
-        size_t card_width_borders = played_card_size.GetWidth() + 8;
-
+        size_t card_width_borders = formatting_constants::DEFAULT_PLAYED_CARD_SIZE.GetWidth() + 8;
+        const size_t num_cards = std::min(cards_size, (size_t)5);
 
         // Create the hand panel
         wxPanel* hand = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
 
         // Create the sizer for the hand
-        wxGridSizer* sizer = new wxGridSizer(1, cards_size, 0, 0);
-        sizer->SetMinSize(wxSize(5 * card_width_borders, 150));
+        wxGridSizer* sizer = new wxGridSizer(1, num_cards, 5, 5);
+        sizer->SetMinSize(wxSize(5 * card_width_borders, 150));        
+
+        const size_t start_idx = cards_size - num_cards;
+        // Add the cards to the hand
+        for (size_t i = start_idx; i < std::max(num_cards, cards_size); i++) {
+            SingleCardPanel* card = new SingleCardPanel(hand, cards[i], formatting_constants::DEFAULT_PLAYED_CARD_SIZE);
+
+            sizer->Add(card, 0, wxALL, 4);
+        }
 
         // Set the sizer for the hand panel
         hand->SetSizer(sizer);
-
-        // Set the size of the cards
-        if (card_width_borders * cards_size > 724) {
-            // scale bigger hands
-            played_card_size.SetWidth(724 / cards_size - 8);
-            played_card_size.SetHeight(played_card_size.GetWidth() / 4 * 5);
-        }
-
-        // Add the cards to the hand
-        for (size_t i = 0; i < cards_size; i++) {
-            ImagePanel* card = new ImagePanel(hand, "assets/" + cards[i] + ".png", wxBITMAP_TYPE_PNG, wxDefaultPosition,
-                played_card_size, 0);
-
-            sizer->Add(card, 0, wxALIGN_CENTER, 4);
-        }
         return hand;
     }
 
@@ -105,16 +98,13 @@ namespace client
         std::vector<shared::CardBase::id_t> cards = {"Village", "Copper", "Copper", "Estate", "Smithy", "Laboratory", "Festival", "Market"};
         // Get the hand cards
         size_t cards_size = cards.size();
-        size_t card_width_borders = played_card_size.GetWidth() + 8;
         const size_t num_cards = std::min(cards_size, (size_t)5);
-
 
         // Create the hand panel
         wxPanel* hand = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
 
         // Create the sizer for the hand
-        wxGridSizer* sizer = new wxGridSizer(1, num_cards, 5, 5);
-        sizer->SetMinSize(wxSize(5 * card_width_borders, 150));        
+        wxGridSizer* sizer = new wxGridSizer(1, num_cards, 5, 5);        
 
         const size_t start_idx = cards_size - num_cards;
         // Add the cards to the hand

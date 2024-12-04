3774de6ce180b07e61d9b5b3a40d02b7155c6069
Philipp <pobrist@student.ethz.ch>
2024-12-04 16:23:12 +0100

    modify SingleCardPanel to make a Border

--- modules/client/src/uiElements/single_card_panel.cpp
+++ modules/client/src/uiElements/single_card_panel.cpp
@@ -4,15 +4,21 @@
 namespace client
 {
     SingleCardPanel::SingleCardPanel(wxWindow *parent, const std::string &card_name, wxSize size, int padding) :
-        wxPanel(parent, wxID_ANY, wxDefaultPosition, wxDefaultSize)
+        wxPanel(parent, wxID_ANY, wxDefaultPosition, wxDefaultSize), CardName(card_name)
     {
         wxBoxSizer *sizer = new wxBoxSizer(wxVERTICAL);
 
         std::string CardPath = "assets/" + card_name + ".png";
 
         Image = new ImagePanel(this, CardPath, wxBITMAP_TYPE_PNG, wxDefaultPosition,
-                               wxSize(size.GetWidth(), size.GetHeight() - 2 * padding));
-        sizer->Add(Image, 0, wxALIGN_CENTER_HORIZONTAL);
+                               wxSize(size.GetWidth() - 2 * padding, size.GetHeight() - 2 * padding));
+        sizer->Add(Image, 0, wxALIGN_CENTER_HORIZONTAL | wxALL, padding);
         this->SetSizer(sizer);
+    }
+
+    void SingleCardPanel::setBorderColor(const wxColour &color)
+    {
+        this->SetBackgroundColour(color); // Set background color
+        this->Refresh(); // Trigger a repaint
     }
 } // namespace client

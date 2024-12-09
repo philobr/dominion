10b9b996711ed5d493e12429192eabba1b10e531
Philipp <pobrist@student.ethz.ch>
2024-12-09 23:00:15 +0100

    formatter

--- modules/client/include/uiElements/formatting_constants.h
+++ modules/client/include/uiElements/formatting_constants.h
@@ -14,12 +14,12 @@ namespace client
         const wxColour ACTIVE_PLAYER_INFO_BACKGROUND = wxColour(200, 240, 200);
         const wxColour PASSIVE_PLAYER_INFO_BACKGROUND = ACTIVE_ENEMY_BACKGROUND; // wxColour(200, 220, 240);
 
-        // Panel formatting constants
-        #ifdef __APPLE__
+// Panel formatting constants
+#ifdef __APPLE__
         const wxSize DEFAULT_BOARD_PILE_SIZE = wxSize(100, 150);
-        #else
+#else
         const wxSize DEFAULT_BOARD_PILE_SIZE = wxSize(80, 120);
-        #endif
+#endif
         const wxSize DEFAULT_PLAYED_CARD_SIZE = wxSize(60, 90);
         const wxSize DEFAULT_CARD_SELECTION_SIZE = wxSize(160, 240);
 

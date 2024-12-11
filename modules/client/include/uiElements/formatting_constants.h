e35f75dfae61ca7e78cb670263332a900b90dd5b
Philipp <pobrist@student.ethz.ch>
2024-12-11 22:04:22 +0100

    formatting

--- modules/client/include/uiElements/formatting_constants.h
+++ modules/client/include/uiElements/formatting_constants.h
@@ -10,9 +10,10 @@ namespace client
         const wxFont DEFAULT_FONT = wxFont(10, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
 
         // Panel background colours
-        const wxColour ENEMY_BACKGROUND = wxColour(255, 200, 200);
-        const wxColour ACTIVE_ENEMY_BACKGROUND = wxColour(255, 200, 150);
-        const wxColour ACTIVE_PLAYER_INFO_BACKGROUND = wxColour(200, 240, 200);
+        const wxColour DEFAULT_WINDOW_BACKGROUND = wxColor(213, 231, 239); // light blue
+        const wxColour ENEMY_BACKGROUND = wxColour(255, 200, 200); // light red
+        const wxColour ACTIVE_ENEMY_BACKGROUND = wxColour(255, 200, 150); // light orange
+        const wxColour ACTIVE_PLAYER_INFO_BACKGROUND = wxColour(95, 100, 55); // light green
         const wxColour PASSIVE_PLAYER_INFO_BACKGROUND = ACTIVE_ENEMY_BACKGROUND; // wxColour(200, 220, 240);
         const wxColour SELECTED_CARD_BACKGROUND = *wxYELLOW;
 
@@ -27,5 +28,10 @@ namespace client
 
         // Border colours
         const wxColour PLAYABLE_HAND_CARD_BORDER = wxColour(0, 130, 0);
+
+        // Window background iamge
+        const wxString WINDOW_BACKGROUND_PATH = "assets/Background.jpg";
+        static bool background_image;
+        const wxColour DEFAULT_PANEL_BACKGROUND = wxColour(255, 200, 100); // light orange
     } // namespace formatting_constants
 } // namespace client

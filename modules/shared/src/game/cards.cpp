32c99cbd1c3331c7ff66d7ec8138a9a1030b0031
amazzetta <amazzetta@student.ethz.ch>
2024-12-13 21:58:32 +0100

    finished

--- modules/shared/src/game/cards.cpp
+++ modules/shared/src/game/cards.cpp
@@ -56,6 +56,7 @@ namespace shared
     REGISTER_CARD(Great_Hall, CardType::ACTION | CardType::VICTORY, 3);
     REGISTER_CARD(Treasure_Map, CardType::ACTION, 4);
     REGISTER_CARD(Sea_Hag, CardType::ACTION | CardType::ATTACK, 4);
+    REGISTER_CARD(Poacher, CardType::ACTION, 4);
 
     // victory cards
     REGISTER_CARD(Gardens, CardType::KINGDOM | CardType::VICTORY, 4);
@@ -74,7 +75,6 @@ namespace shared
     REGISTER_CARD(Workshop, CardType::ACTION, 3);
     // REGISTER_CARD(Vassal, CardType::ACTION, 3);
     // REGISTER_CARD(Harbinger, CardType::ACTION, 3);
-    // REGISTER_CARD(Poacher, CardType::ACTION, 4);
     // REGISTER_CARD(Militia, CardType::ACTION | CardType::ATTACK, 4);
     // REGISTER_CARD(Moat, CardType::ACTION | CardType::REACTION, 2);
     //  REGISTER_CARD(Workshop, CardType::ACTION, 3);
